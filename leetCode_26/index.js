d928330fa714219082c4c298f81d0b1160fa0544
Ismail415343 <muhammadmusa12689@gmail.com>
2026-02-06 15:19:12 +0500

    .

---
+++ leetCode_26/index.js
@@ -0,0 +1,17 @@
+let removeDuplicate = (nums)=>{
+
+    let slow = 0;
+    for(let fast = 1; fast < nums.length; fast++){
+      if(nums[fast] !==nums[slow]){
+        slow++;
+        nums[slow]=nums[fast];
+
+      }
+    }
+    return slow + 1;
+}
+
+let nums = [1,1,2,2,3,4,5,5]
+let k = removeDuplicate(nums);
+console.log(k);
+console.log(nums)
